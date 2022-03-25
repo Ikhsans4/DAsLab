91812b4c978d5e5254d8e839c1c9687d6e55b8fe
ikhsan <m_ikhsan20@mhs.unsyiah.ac.id>
2022-03-25 22:58:41 +0700

    add preloader

--- resources/views/Layout/main.blade.php
+++ resources/views/Layout/main.blade.php
@@ -34,9 +34,9 @@
     <div class="wrapper">
 
         <!-- Preloader -->
-        {{-- <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
-        </div> --}}
+        <div class="preloader flex-column justify-content-center align-items-center">
+            <img class="animation__shake" src="{{ url('/img/1.png') }}" alt="D'AsLab" height="60" width="60">
+        </div>
 
         <!-- Navbar -->
         <nav class="main-header navbar navbar-expand navbar-white navbar-dark" style="background-color: #272A37;">
