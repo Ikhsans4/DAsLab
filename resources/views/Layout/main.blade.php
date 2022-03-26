441a60ac3d1e93160c60efe6c48e9eb52f124c95
ikhsan <m_ikhsan20@mhs.unsyiah.ac.id>
2022-03-26 21:16:27 +0700

    fix icon

--- resources/views/Layout/main.blade.php
+++ resources/views/Layout/main.blade.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
 
+    <!-- Fav icon -->
+    <link rel="icon" type="image/x-icon" href="{{ url('img/icon/uskLogo.png') }}" style="width: 2px">
+
+
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -60,8 +64,8 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4" style="background-color: #272A37;">
             <!-- Brand Logo -->
             <a href="{{ url('/admin/dashboard') }}" class="brand-link">
-                <img src="{{ url('img/uskLogo.png') }}" alt="usk logo" class="brand-image img-circle elevation-3"
-                    style="opacity: .8">
+                <img src="{{ url('img/icon/uskLogo.png') }}" alt="usk logo"
+                    class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">D'AsLab</span>
             </a>
 
--- resources/views/Layout/user.blade.php
+++ resources/views/Layout/user.blade.php
@@ -8,7 +8,7 @@
 
 
     <!-- Fav icon -->
-    <link rel="icon" type="image/x-icon" href="{{ url('img/uskLogo.png') }}" style="width: 2px">
+    <link rel="icon" type="image/x-icon" href="{{ url('img/icon/uskLogo.png') }}" style="width: 2px">
 
 
     <!-- Google Font: Source Sans Pro -->
@@ -64,8 +64,8 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4" style="background-color: #272A37;">
             <!-- Brand Logo -->
             <a href="{{ url('/') }}" class="brand-link">
-                <img src="{{ url('img/uskLogo.png') }}" alt="usk logo" class="brand-image img-circle elevation-3"
-                    style="opacity: .8">
+                <img src="{{ url('img/icon/uskLogo.png') }}" alt="usk logo"
+                    class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">D'AsLab</span>
             </a>
 
