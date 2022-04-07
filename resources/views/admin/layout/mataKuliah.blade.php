b7c0b92cc2bdb120a4b78e6dea8dcf01f382c9ea
ikhsan <m_ikhsan20@mhs.unsyiah.ac.id>
2022-04-07 15:56:04 +0700

    conflict fix

--- resources/views/admin/layout/mataKuliah.blade.php
+++ resources/views/admin/layout/mataKuliah.blade.php
@@ -9,28 +9,26 @@
 
 
     <section class="content text-dark" id="list-mk" style="background-color: #272A37;">
-        <div class="d-flex flex-wrap text-dark">
-            @foreach ($mata_kuliah as $mk)
-                <div class="card mx-2" style="width: 18rem;">
-                    <img src="{{ url('img/matakuliah/tag.jpeg') }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $mk['mata_kuliah'] }}</h5>
-                        <p class="card-text">
-                            Asisten : <br>
-                            @foreach ($asisten as $aslab)
-                                @if ($aslab['mataKuliah'] === $mk['mata_kuliah'])
-                                    {{ $loop->iteration }}. {{ $aslab['nama'] }} <br>
-                                @else
-                                    Tidak ada asisten yang mendaftar
-                                @break
-                            @endif
-                        @endforeach
-                    </p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
+        @foreach ($mata_kuliah as $mk)
+            <div class="card" style="width: 18rem;">
+                <img src="" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $mk['mata_kuliah'] }}</h5>
+                    <p class="card-text">
+                        Asisten : <br>
+                        @foreach ($asisten as $aslab)
+                            @if ($aslab['mataKuliah'] === $mk['mata_kuliah'])
+                                {{ $loop->iteration }}. {{ $aslab['nama'] }} <br>
+                            @else
+                                Tidak ada asisten yang mendaftar
+                            @break
+                        @endif
+                    @endforeach
+                </p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
-        @endforeach
-    </div>
+        </div>
+    @endforeach
 
 
 
