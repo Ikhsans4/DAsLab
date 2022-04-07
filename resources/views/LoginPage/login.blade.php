788ae7da10dba36b39f8c909c9659b84aa9ae1e1
ikhsan <m_ikhsan20@mhs.unsyiah.ac.id>
2022-04-07 22:50:22 +0700

    change login image

--- resources/views/LoginPage/login.blade.php
+++ resources/views/LoginPage/login.blade.php
@@ -10,7 +10,8 @@
     <link rel="icon" type="image/x-icon" href="{{ url('img/icon/uskLogo.png') }}" style="width: 2px">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <!-- admin LTE -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
@@ -27,7 +28,8 @@
     <div class="container-fluid">
         <div class="row" style="position: fixed; bottom: 0; top:0; left: 0;right: 0;">
             <div class="col-md-6" style="background-color: #272a37; color: white;">
-                <img src="{{ url('img/icon/uskLogo.png') }}" class="float-start mt-2 mx-3" style="width: 65px;" alt="USK Logo">
+                <img src="{{ url('img/icon/uskLogo.png') }}" class="float-start mt-2 mx-3" style="width: 65px;"
+                    alt="USK Logo">
                 <div class="pt-4" style="margin-top: 7px">
                     <h5>D'AsLab</h5>
                 </div>
@@ -39,11 +41,13 @@
                         </div>
                         <div class="mb-3">
                             <label for="npm" class="form-label">NPM</label>
-                            <input type="tel" class="form-control r-10" id="npm" name="npm" placeholder="NPM" style="width: 400px;" autofocus required>
+                            <input type="tel" class="form-control r-10" id="npm" name="npm" placeholder="NPM"
+                                style="width: 400px;" autofocus required>
                         </div>
                         <div class="mb-3">
                             <label for="password" class="form-label">Password</label>
-                            <input type="password" class="form-control r-10" id="password" name="password" placeholder="Password" required>
+                            <input type="password" class="form-control r-10" id="password" name="password"
+                                placeholder="Password" required>
                         </div>
                         <div class="text-center" style="margin-top: 2em;">
                             <button type="submit" class="btn btn-primary padd-button rounded-pill">LOGIN</button>
@@ -54,8 +58,7 @@
                 {{-- <div class="d-flex justify-content-center col">
                 </div> --}}
             </div>
-            <div class="col-md-6 opacity-25" style="background-image : url(img/login.jpg); background-size: cover;">
-                {{-- <img src="{{ asset('img/login.jpg') }}" > --}}
+            <div class="col-md-6" style="background-image :  url('img/login.jpg'); background-size: cover;">
             </div>
 
         </div>
@@ -64,7 +67,8 @@
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
