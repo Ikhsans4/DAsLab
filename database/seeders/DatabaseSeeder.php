229d12980a25a06d4feb98c3a43434440d6d9b6f
daffa1134 <daffa1134@gmail.com>
2022-04-11 00:21:56 +0700

    Seed user random

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(10)->create();
+        User::factory(5)->create();
 
         Matakuliah::create([
             'kode_mk' => 'INF108',
