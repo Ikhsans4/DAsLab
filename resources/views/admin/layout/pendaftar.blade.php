76786df7597d5b02af0fa850385dd3a9017b0c07
ikhsan <m_ikhsan20@mhs.unsyiah.ac.id>
2022-04-24 20:55:45 +0700

    fix tag at pendaftar page

--- resources/views/admin/layout/pendaftar.blade.php
+++ resources/views/admin/layout/pendaftar.blade.php
@@ -59,58 +59,59 @@
             </tbody>
 
         </table>
+    </section>
 
-        <!-- /.tabel -->
-    @endsection
+    <!-- /.tabel -->
+@endsection
 
-    @section('table')
+@section('table')
 
-        <!-- DataTables  & Plugins -->
-        <script src="{{ url('plugins/datatables/jquery.dataTables.min.js') }}"></script>
-        <script src="{{ url('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-        <script src="{{ url('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
-        <script src="{{ url('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
-        <script src="{{ url('plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
-        <script src="{{ url('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
-        <script src="{{ url('plugins/jszip/jszip.min.js') }}"></script>
-        <script src="{{ url('plugins/pdfmake/pdfmake.min.js') }}"></script>
-        <script src="{{ url('plugins/pdfmake/vfs_fonts.js') }}"></script>
-        <script src="{{ url('plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
-        <script src="{{ url('plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
-        <script src="{{ url('plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
-        <!-- Sweet Alert -->
-        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <!-- DataTables  & Plugins -->
+    <script src="{{ url('plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ url('plugins/jszip/jszip.min.js') }}"></script>
+    <script src="{{ url('plugins/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ url('plugins/pdfmake/vfs_fonts.js') }}"></script>
+    <script src="{{ url('plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+    <script src="{{ url('plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+    <!-- Sweet Alert -->
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
-        <!-- Page specific script -->
-        <script>
-            $(function() {
-                $('#example1').DataTable({
-                    "paging": true,
-                    "lengthChange": false,
-                    "searching": true,
-                    "ordering": true,
-                    "info": true,
-                    "autoWidth": true,
-                    "responsive": true,
-                });
+    <!-- Page specific script -->
+    <script>
+        $(function() {
+            $('#example1').DataTable({
+                "paging": true,
+                "lengthChange": false,
+                "searching": true,
+                "ordering": true,
+                "info": true,
+                "autoWidth": true,
+                "responsive": true,
             });
-        </script>
+        });
+    </script>
 
-        <script>
-            function confirm() {
-                Swal.fire({
-                    title: 'Do you want to save the changes?',
-                    showDenyButton: true,
-                    showCancelButton: true,
-                    confirmButtonText: 'Save',
-                }).then((result) => {
-                    /* Read more about isConfirmed, isDenied below */
-                    if (result.isConfirmed) {
-                        Swal.fire('Saved!', '', 'success')
-                    } else if (result.isDenied) {
-                        Swal.fire('Changes are not saved', '', 'info')
-                    }
-                })
-            }
-        </script>
-    @endsection
+    <script>
+        function confirm() {
+            Swal.fire({
+                title: 'Do you want to save the changes?',
+                showDenyButton: true,
+                showCancelButton: true,
+                confirmButtonText: 'Save',
+            }).then((result) => {
+                /* Read more about isConfirmed, isDenied below */
+                if (result.isConfirmed) {
+                    Swal.fire('Saved!', '', 'success')
+                } else if (result.isDenied) {
+                    Swal.fire('Changes are not saved', '', 'info')
+                }
+            })
+        }
+    </script>
+@endsection
