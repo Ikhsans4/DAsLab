a22013f5c71b27be44aa3d858753660c1799f0a0
ikhsan <m_ikhsan20@mhs.unsyiah.ac.id>
2022-05-10 15:13:23 +0700

    siap conflict

--- resources/views/user/layout/mataKuliah.blade.php
+++ resources/views/user/layout/mataKuliah.blade.php
@@ -5,39 +5,25 @@
 
     <section class="content" style="background-color: #272A37;">
 
-    <div class="container-fluid d-flex flex-wrap justify-content-center text-dark mt-3">
-        <!-- card -->
-        @foreach ($mata_kuliah as $mk)
-        <div class="card mb-2" style="width: 18rem; padding: 1rem; margin: 5px;">
-            <img src="{{ url($mk['link_pic']) }}" class="card-img-top" alt="mk_picture" style="height: 150px; width: auto">
-            <div class="card-body">
-                <h5 class="card-title"><b>{{ $mk['nama_mk'] }}</b></h5>
-                <p class="card-text">
-                    Dosen Pengampu : <br> 
-                    @foreach ($dosen as $dsn)
-                        @if ($mk['nip_dosen'] === $dsn['nip'])
-                            {{ $dsn['nama'] }}
-                        @endif
-                    @endforeach
-                    @php
-                        $i = 1;
-                        $status = 0;
-                    @endphp
-                    Asisten : <br>
-                    @foreach ($asisten as $aslab)                  
-                            @if ($aslab['status'] === 1 && $mk['nama_mk'] === $aslab['mataKuliah'])
-                                {{ $i }}. {{ $aslab['nama'] }}
-                                @php
-                                    $i += 1;
-                                    $status = 1;
-                                @endphp
-                            @endif
-                    @endforeach
-                    @if ($status !== 1)
-                        Tidak ada Asisten
-                    @endif
-                </p>
-            </div>
+        <div class="container-fluid d-flex flex-wrap justify-content-center text-dark mt-3">
+            <!-- card -->
+            @foreach ($mata_kuliah as $mk)
+                <div class="card mb-2" style="width: 18rem; padding: 1rem; margin: 5px;">
+                    <img src="{{ url($mk['link_pic']) }}" class="card-img-top" alt="mk_picture"
+                        style="height: 150px; width: auto">
+                    <div class="card-body">
+                        <h5 class="card-title"><b>{{ $mk['nama_mk'] }}</b></h5>
+                        <p class="card-text">
+                            @foreach ($dosen as $dsn)
+                                @if ($mk['nip_dosen'] === $dsn['nip'])
+                                    Dosen Pengampu : <br> {{ $dsn['nama'] }}
+                                @endif
+                            @endforeach
+                        </p>
+                    </div>
+                </div>
+                <!-- end card -->
+            @endforeach
         </div>
     </section>
 
