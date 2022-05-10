17fd8378d6635fd7785447200df5d298d6c84124
daffa1134 <daffa1134@gmail.com>
2022-05-10 14:29:28 +0700

    fix problem user mk

--- resources/views/user/layout/mataKuliah.blade.php
+++ resources/views/user/layout/mataKuliah.blade.php
@@ -1,10 +1,9 @@
 @extends('main.layout.user')
 @section('title', "D'AsLab | Mata Kuliah")
 @section('head', 'Mata Kuliah')
+
 @section('container')
-
-    <section class="content" style="background-color: #272A37;">
-
+<section class="content" style="background-color: #272A37;">
     <div class="container-fluid d-flex flex-wrap justify-content-center text-dark mt-3">
         <!-- card -->
         @foreach ($mata_kuliah as $mk)
@@ -13,7 +12,7 @@
             <div class="card-body">
                 <h5 class="card-title"><b>{{ $mk['nama_mk'] }}</b></h5>
                 <p class="card-text">
-                    Dosen Pengampu : <br> 
+                    Dosen Pengampu : <br>
                     @foreach ($dosen as $dsn)
                         @if ($mk['nip_dosen'] === $dsn['nip'])
                             {{ $dsn['nama'] }}
@@ -23,23 +22,24 @@
                         $i = 1;
                         $status = 0;
                     @endphp
-                    Asisten : <br>
-                    @foreach ($asisten as $aslab)                  
-                            @if ($aslab['status'] === 1 && $mk['nama_mk'] === $aslab['mataKuliah'])
-                                {{ $i }}. {{ $aslab['nama'] }}
-                                @php
-                                    $i += 1;
-                                    $status = 1;
-                                @endphp
-                            @endif
+                    <br>Asisten : <br>
+                    @foreach ($asisten as $aslab)
+                        @if ($aslab['status'] === 1 && $mk['nama_mk'] === $aslab['mataKuliah'])
+                            {{ $i }}. {{ $aslab['nama'] }}
+                            @php
+                                $i += 1;
+                                $status = 1;
+                            @endphp
+                        @endif
                     @endforeach
+                    
                     @if ($status !== 1)
                         Tidak ada Asisten
                     @endif
                 </p>
             </div>
         </div>
-    </section>
-
-
+        @endforeach
+    </div>
+</section>
 @endsection
