7fa7efc3ba7581cae6738c9e8411e17f0733a072
ikhsan <m_ikhsan20@mhs.unsyiah.ac.id>
2022-05-10 11:54:43 +0700

    show mk sesuai jurusan user

--- resources/views/user/layout/mataKuliah.blade.php
+++ resources/views/user/layout/mataKuliah.blade.php
@@ -3,28 +3,31 @@
 @section('head', 'Mata Kuliah')
 @section('container')
 
-<section class="content" style="background-color: #272A37;">
+    <section class="content" style="background-color: #272A37;">
 
-    <div class="container-fluid d-flex flex-wrap justify-content-center text-dark mt-3">
-        <!-- card -->
-        @foreach ($mata_kuliah as $mk)
-        <div class="card mb-2" style="width: 18rem; padding: 1rem; margin: 5px;">
-            <img src="{{ url($mk['link_pic']) }}" class="card-img-top" alt="mk_picture" style="height: 150px; width: auto">
-            <div class="card-body">
-                <h5 class="card-title"><b>{{ $mk['nama_mk'] }}</b></h5>
-                <p class="card-text">
-                    @foreach ($dosen as $dsn)
-                        @if ($mk['nip_dosen'] === $dsn['nip'])
-                        Dosen Pengampu : <br> {{ $dsn['nama'] }}
-                        @endif
-                    @endforeach
-                </p>
-            </div>
+        <div class="container-fluid d-flex flex-wrap justify-content-center text-dark mt-3">
+            @foreach ($mata_kuliah as $mk)
+                @if ($mk['jurusan'] === auth()->user()->jurusan)
+                    <!-- card -->
+                    <div class="card mb-2" style="width: 18rem; padding: 1rem; margin: 5px;">
+                        <img src="{{ url($mk['link_pic']) }}" class="card-img-top" alt="mk_picture"
+                            style="height: 150px; width: auto">
+                        <div class="card-body">
+                            <h5 class="card-title"><b>{{ $mk['nama_mk'] }}</b></h5>
+                            <p class="card-text">
+                                @foreach ($dosen as $dsn)
+                                    @if ($mk['nip_dosen'] === $dsn['nip'])
+                                        Dosen Pengampu : <br> {{ $dsn['nama'] }}
+                                    @endif
+                                @endforeach
+                            </p>
+                        </div>
+                    </div>
+                    <!-- end card -->
+                @endif
+            @endforeach
         </div>
-        <!-- end card -->
-        @endforeach
-    </div>
-</section>
+    </section>
 
 
 @endsection
