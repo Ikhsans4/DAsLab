2de7a795831b8b02efa2e22adb3ef97b2ca87a0a
ikhsan <m_ikhsan20@mhs.unsyiah.ac.id>
2022-05-13 15:09:09 +0700

    fix error when login

--- resources/views/login/layout/login.blade.php
+++ resources/views/login/layout/login.blade.php
@@ -105,32 +105,34 @@
         </script>
     @endif
 
-    <script>
-        $('.login').click(function() {
-            let timerInterval
-            Swal.fire({
-                title: 'Proses Login!',
-                // html: 'I will close in <b></b> milliseconds.',
-                timer: 2000,
-                timerProgressBar: true,
-                didOpen: () => {
-                    Swal.showLoading()
-                    const b = Swal.getHtmlContainer().querySelector('b')
-                    timerInterval = setInterval(() => {
-                        b.textContent = Swal.getTimerLeft()
-                    }, 100)
-                },
-                willClose: () => {
-                    clearInterval(timerInterval)
-                }
-            }).then((result) => {
-                /* Read more about handling dismissals below */
-                if (result.dismiss === Swal.DismissReason.timer) {
-                    console.log('I was closed by the timer')
-                }
-            })
-        });
-    </script>
+    {{-- @if (isset($npm))
+        <script>
+            $('.login').click(function() {
+                let timerInterval
+                Swal.fire({
+                    title: 'Proses Login!',
+                    // html: 'I will close in <b></b> milliseconds.',
+                    timer: 2000,
+                    timerProgressBar: true,
+                    didOpen: () => {
+                        Swal.showLoading()
+                        const b = Swal.getHtmlContainer().querySelector('b')
+                        timerInterval = setInterval(() => {
+                            b.textContent = Swal.getTimerLeft()
+                        }, 100)
+                    },
+                    willClose: () => {
+                        clearInterval(timerInterval)
+                    }
+                }).then((result) => {
+                    /* Read more about handling dismissals below */
+                    if (result.dismiss === Swal.DismissReason.timer) {
+                        console.log('I was closed by the timer')
+                    }
+                })
+            });
+        </script>
+    @endif --}}
 
 
 
