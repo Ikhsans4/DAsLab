813b564d36e981e4abffc184d1161bb1a6847bfd
daffa1134 <daffa1134@gmail.com>
2022-05-13 15:31:22 +0700

    add seeder dosen

--- database/seeders/DosenSeeder.php
+++ database/seeders/DosenSeeder.php
@@ -74,5 +74,185 @@ class DosenSeeder extends Seeder
             'nama' => 'Dr. Lelifajri, S.Si, M.Si',
             'email' => 'lelifajri@unsyiah.ac.id'
         ]);
+
+        Dosen::create([
+            'nip' => '197303311998021001',
+            'nama' => 'Muhammad Nasir, S.Si., M.Sc',
+            'email' => 'm_nasir@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '197507172000122001',
+            'nama' => 'Dr. Yulia Sari Ismail, S.Si., M.Si',
+            'email' => 'ysismail@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '198202032008122001',
+            'nama' => 'Alia Rizki, S.Si, M.Sc',
+            'email' => 'alia_rizki@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '197112121998032001',
+            'nama' => 'Cut Nanda Defira, S.Si , M.Si',
+            'email' => 'cutnandadevira@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '198207282006042002',
+            'nama' => 'Lenni Fitri, S.Si.,MP',
+            'email' => 'lennifitri@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '197503212008121001',
+            'nama' => 'Masykur, S.Si., M.Sc',
+            'email' => 'masykur@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '198109252008122002',
+            'nama' => 'Lydia Septa Desiyana, S.Farm., M.Si.Apt',
+            'email' => 'lydiasepta.desiyana@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '198101182006042001',
+            'nama' => 'Cut Yulvizar, S.Si, M.Sc',
+            'email' => 'cut_yulvizar@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '197711072010122001',
+            'nama' => 'Misrahanum, S.Si., M.Kes',
+            'email' => 'misra.hanum@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '197602062009121002',
+            'nama' => 'Sadli, ST., M.Si',
+            'email' => 'sadli_zhafara@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '198203232012122001',
+            'nama' => 'Irma Sari, M.Si., Apt',
+            'email' => 'irmasari@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '198505022014042002',
+            'nama' => 'Hilda Maysarah, S.Farm., Apt., M.Si.',
+            'email' => 'hildamaysarah@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '197107061994121001',
+            'nama' => 'Dr. Suhrawardi Ilyas, S.Si, M.Sc',
+            'email' => 'suhra.ilyas@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '196712241994031001',
+            'nama' => 'Dr. Syahrun Nur, S.Si., M.Sc',
+            'email' => 'syahrun_madjid@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '196609281994031014',
+            'nama' => 'Fauzi, S.Si, M.Si',
+            'email' => 'fauzi@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '197208311998021001',
+            'nama' => 'Rajibussalim, S.Si, M.InfoTech',
+            'email' => 'rajibussalim@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '197312072003121002',
+            'nama' => 'Dr. Didik Sugiyanto, M.T.',
+            'email' => 'didiksg@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '196102011997021001',
+            'nama' => 'Drs. Kurnia Lahna, M.T.',
+            'email' => 'kurnialahna@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '198508142014042001',
+            'nama' => 'Radhiah, S.Si., M.Sc.',
+            'email' => 'radhiah@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '197010121995122002',
+            'nama' => 'Rini Oktavia, S.Si., M.Si',
+            'email' => 'rini_oktavia@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '197506091999032001',
+            'nama' => 'Rahma Zuhra, S.Si., M.Si',
+            'email' => 'rahmazuhra@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '197006081994122001',
+            'nama' => 'Nurmaulidar, S.Si., M.Sc',
+            'email' => 'lidar@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '197004071995121001',
+            'nama' => 'Taufiq Iskandar, S.Si., M.Si',
+            'email' => 'taufiqiskandar@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '198006072008122001',
+            'nama' => 'Syarifah Meurah Yuni, S.Si., M.Si',
+            'email' => 'sy.meurah.yuni@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '197410152006042002',
+            'nama' => 'Fitriana AR, S.Si., M.Si',
+            'email' => 'fithriana.ar@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '196805191994031012',
+            'nama' => 'Dr. Saiful Mahdi, S.Si, M.Sc',
+            'email' => 'saiful.mahdi@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '197405252000031004',
+            'nama' => 'Miftahuddin, S.Si, M.Si',
+            'email' => 'miftah@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '198409282015042002',
+            'nama' => 'Latifah Rahayu Siregar, S.Si., M.Sc.',
+            'email' => 'latifah.rahayu@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '197309281998022001',
+            'nama' => 'Evi Ramadhani, S.Si.,M.Si',
+            'email' => 'evi.ramadhani@unsyiah.ac.id',
+        ]);
+        
+        Dosen::create([
+            'nip' => '197603172006041001',
+            'nama' => 'Asep Rusyana, S.Si., M.Si',
+            'email' => 'asep.rusyana@unsyiah.ac.id',
+        ]);
     }
 }
